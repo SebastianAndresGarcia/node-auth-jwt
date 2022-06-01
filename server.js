b786081b99aa662183b28106998b915dbdb676e3
SebastianAndresGarcia <sebastiana.garcia@alumnos.frm.utn.edu.ar>
2022-06-01 11:48:52 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+//https://www.bezkoder.com/react-express-authentication-jwt/
+//https://www.bezkoder.com/node-js-jwt-authentication-mysql/
+//para el front https://www.bezkoder.com/react-jwt-auth/
+//https://www.bezkoder.com/react-hooks-jwt-auth/
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:8081"
+  };
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+const db = require("./app/models");
+const Role = db.role;
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+  });
+  
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to udapif application." });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
